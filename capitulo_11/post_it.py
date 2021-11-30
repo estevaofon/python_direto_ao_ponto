7465622ff31ecc969739f363f5a7773f4e96340b
estevaofon <estevaopfon@gmail.com>
2021-11-30 20:24:11 -0300

    atualizado post_it.py capitulo 11

--- capitulo_11/post_it.py
+++ capitulo_11/post_it.py
@@ -1,38 +1,62 @@
 import os
-notes = []
-sair = False
 
-try:
-    with open("notas.txt", 'r') as file:
-        notes = file.readlines()
-        # Remove os \n
-        notes = [note.rstrip() for note in notes]
-except FileNotFoundError:
-    pass
-while not sair:
-    resp = input('Fazer anotação? (s/n)')
-    if resp == "s":
-        text = input("Anotação: ")
-        notes.append(text)
+def anotacoes_para_lista(nome_do_arquivo):
+    """
+    Abre o arquivo com as anotações
+    Lê as anotações e as retorna em uma lista
+    Caso o arquivo ainda não exista 
+    retorna uma lista vazia
+    """
+    try:
+        with open(nome_do_arquivo, 'r') as file:
+            notes = file.readlines()
+            # Remove os \n com uma list comprehension
+            notes = [note.rstrip() for note in notes]
+            return notes
+    except FileNotFoundError:
+        notes = []
+        return notes
 
-    print('\n'+"Anotações".center(30, "="))
-    for note in notes:
+def salvar_anotacoes(nome_do_arquivo, notes):
+    """Escreve as anotações em um arquivo
+    Cada anotação em uma linha
+    """
+    with open(nome_do_arquivo, 'w') as file:
+        # Adiciona os \n para salvar uma nota em cada linha
+        notes = [note+'\n' for note in notes]
+        file.writelines(notes)
 
-        text_size = len(note)
-        note_size = text_size+6
-        print('-'*note_size)
-        print(' '*note_size)
-        print(note.center(note_size))
-        print(' '*note_size)
-        print('-'*note_size)
-        print()
+def layout_da_nota(note):
+    """Aparência da anotação"""
+    text_size = len(note)
+    note_size = text_size+6
+    print('-'*note_size)
+    print(' '*note_size)
+    print(note.center(note_size))
+    print(' '*note_size)
+    print('-'*note_size)
+    print()
 
-    r = input("sair? (s/n)")
-    if r == "s":
-        sair = True
-        with open("notas.txt", 'w') as file:
-            # Adiciona os \n para salvar uma nota em cada linha
-            notes = [note+'\n' for note in notes]
-            file.writelines(notes)
-    # limpa a tela
-    os.system('cls' if os.name == 'nt' else 'clear')
+def rodar():
+    sair = False
+    notes = anotacoes_para_lista("notas.txt")
+
+    while not sair:
+        resposta = input('Fazer anotação? (s/n)')
+        if resposta == "s":
+            text = input("Anotação: ")
+            notes.append(text)
+
+        print('\n'+"Anotações".center(30, "="))
+        for note in notes:
+            # Exibe as anotações bonitinhas
+            layout_da_nota(note)
+
+        resposta = input("sair? (s/n)")
+        if resposta == "s":
+            sair = True
+            salvar_anotacoes("notas.txt", notes)
+        # limpa a tela
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+rodar()
